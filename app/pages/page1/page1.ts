272bf0e6d005a37801d3a27f19fcaa425e9c5eae
Josï¿½ Guitart <jguitart@opentrends.net>
2016-04-03 18:04:04 +0200

    Pruebas de estilo

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -6,13 +6,11 @@ import {JbcnService} from '../../services/jbcn.service';
   providers:[JbcnService]
 })
 export class Page1 {
-    schedule;
-    groups;
-    date;
+    schedule = [];
+    
     constructor(jbcnService:JbcnService) {
         jbcnService.load().then(data => {
-            this.groups = data.schedule[0].groups;
-            this.date = data.schedule[0].date;
+            this.schedule = data.schedule;
         });
     }
 }
