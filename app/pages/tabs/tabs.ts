e5bd25fd00f8bbfb92003a4fac523270e0193b34
Josï¿½ Guitart <jguitart@opentrends.net>
2016-04-04 18:09:34 +0200

    Reproduciendo el comportamiento original

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import {Page} from 'ionic-angular';
-import {Page1} from '../page1/page1';
-import {Page2} from '../page2/page2';
+import {Schedule} from '../schedule/schedule';
+import {Speakers} from '../speakers/speakers';
 import {Page3} from '../page3/page3';
 
 
@@ -10,7 +10,7 @@ import {Page3} from '../page3/page3';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = Page1;
-  tab2Root: any = Page2;
+  tab1Root: any = Schedule;
+  tab2Root: any = Speakers;
   tab3Root: any = Page3;
 }
